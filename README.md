bbe8c1d03ea5bde836364cec8eb09cb461106b14
eldenhowes <williameldenhowes@gmail.com>
2019-11-15 11:51:30 +0000

    Kata solved

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # vowel_count_java
+
+My solution to this codewars kata: https://www.codewars.com/kata/vowel-count/train/java
--- src/app/Vowels.java
+++ src/app/Vowels.java
@@ -1,7 +1,22 @@
 package app;
 
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello Java");
+import java.util.Arrays;
+import java.util.List;
+
+public class Vowels {
+
+    public static int getCount(String str) {
+        String vowels[] = "aeiou".split("");
+        List<String> vowelsList = Arrays.asList(vowels);
+        String strAsArray[] = str.split("");
+        int strLength = strAsArray.length;
+        int vowelsCount = 0;
+        for (int i = 0; i < strLength; i++) {
+            if (vowelsList.contains(strAsArray[i])) {
+                vowelsCount++;
+            }
+        }
+        return vowelsCount;
     }
+
 }
